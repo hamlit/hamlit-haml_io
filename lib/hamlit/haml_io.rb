dc783d8fe5237b6fa72be533019393d094d43199
Takashi Kokubun <takashikkbn@gmail.com>
2019-12-02 23:13:02 -0800

    bundle gem hamlit-haml_io

---
+++ lib/hamlit/haml_io.rb
@@ -0,0 +1,8 @@
+require "hamlit/haml_io/version"
+
+module Hamlit
+  module HamlIo
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/hamlit/haml_io/version.rb
@@ -0,0 +1,5 @@
+module Hamlit
+  module HamlIo
+    VERSION = "0.1.0"
+  end
+end
